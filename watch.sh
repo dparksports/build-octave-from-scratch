a02bc6efade4f50069e6f2c2a042e870c25ce546
Dan Park <dpark.sports@gmail.com>
2020-10-05 20:40:35 -0700

    adds timestamp

--- watch.sh
+++ watch.sh
@@ -1,6 +1,14 @@
-# for mac only, for lacking watch
+adddate() {
+    while IFS= read -r line; do
+        printf '%s %s\n' "$(date)" "$line";
+    done
+}
+
 while :;
   do
-  echo "$(date)" >> arp.out && arp -i bridge100 -a >> arp.out
+  arp -i bridge100 -a | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> arp.log
+  tail -n 1 arp.log | grep "FF:" | grep -q '.' && afplay /System/Library/Sounds/Purr.aiff
+  tail -n 1 arp.log | grep "incomplete" | grep -q '.' && afplay /System/Library/Sounds/Pop.aiff
   sleep 1
 done
+
